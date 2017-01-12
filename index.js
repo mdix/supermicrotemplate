ae96c87a82363abb9979937113fdf1380346c0be
marc.dix <marcdix@marcdix.de>
2017-01-12 07:34:02 +0100

    adding UMD support. Resolves #1

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-module.exports = (template, values) => {
-    Object.keys(values).forEach((placeholder) => {
-        template = template.replace(new RegExp(placeholder, "g"), values[placeholder])
-    })
-    return template;
-}
+(function(root, factory) {
+    if(typeof define === "function" && define.amd) {
+        define([], factory)
+    } else if(typeof module === "object" && module.exports) {
+        module.exports = factory()
+    } else {
+        root.supermicrotemplate = factory()
+    }
+}(this, function() {
+    return (template, values) => {
+        Object.keys(values).forEach((placeholder) => {
+            template = template.replace(new RegExp(placeholder, "g"), values[placeholder])
+        })
+        return template
+    }
+}))
